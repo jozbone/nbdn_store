e73d7d690c19bb7f3c15290aa1c23fa9960e49a2
Patrick <pconway@ajmpc.com>
2010-10-14 10:50:28 -0600

    Attempt two

--- product/nothinbutdotnetstore.specs/web/ResponseEngineSpecs.cs
+++ product/nothinbutdotnetstore.specs/web/ResponseEngineSpecs.cs
@@ -1,8 +1,7 @@
+ using System.Collections.Generic;
  using Machine.Specifications;
  using Machine.Specifications.DevelopWithPassion.Rhino; 
- using Machine.Specifications.DevelopWithPassion.Extensions;
  using nothinbutdotnetstore.web.infrastructure.frontcontroller;
- using Rhino.Mocks;
  
 namespace nothinbutdotnetstore.specs.web
 {   
@@ -11,15 +10,28 @@ namespace nothinbutdotnetstore.specs.web
         public abstract class concern : Observes<ResponseEngine,
                                             BasicResponseEngine>
         {
-        
+            protected static IEnumerable<string> data;
+            protected static View view;
+
+            Establish c = () =>
+            {
+                data = an<IEnumerable<string>>();
+                view = an<View>();
+
+
+            };
         }
 
         [Subject(typeof(BasicResponseEngine))]
         public class when_displaying_a_view_model : concern
         {
+            Because b = () =>
+                sut.display(data);
 
-            It should_pass_the_model_data_to_the_view;
-            
+            It should_pass_the_model_data_to_the_view = () =>
+                view.received(x => x.Apply(data));
+
+
         }
     }
 }
