e64d3209931d44ef0a357df9eb3066c9b8e038c0
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-15 09:18:40 -0600

    Working on the container static gateway

---
+++ product/nothinbutdotnetstore.specs/infrastructure/ContainerSpecs.cs
@@ -0,0 +1,36 @@
+ 
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
+
+namespace nothinbutdotnetstore.specs.infrastructure
+{
+    public class ContainerSpecs
+    {
+        public abstract class concern : Observes
+        {
+
+        }
+
+        [Subject(typeof(Container))]
+        public class when_requesting_container_servcies : concern
+        {
+            Establish c = () =>
+            {
+                the_underlying_container = an<DependencyContainer>();
+                ContainerResolver resolver = () => the_underlying_container;
+                change(() => Container.container_resolver).to(resolver);
+            };
+
+            Because b = () =>
+                result = Container.resolve;
+
+            It should_return_the_gateway_to_the_actual_container = () =>
+                result.ShouldEqual(the_underlying_container);
+
+
+            static DependencyContainer result;
+            static DependencyContainer the_underlying_container;
+        }
+    }
+}
