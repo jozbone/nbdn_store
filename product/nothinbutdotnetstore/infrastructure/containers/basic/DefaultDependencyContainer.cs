b9166a3ee8ef527cc748d00fb9da9b9a1a87aa8c
Patrick <pconway@ajmpc.com>
2010-10-15 10:32:22 -0600

    Dependency Container Test 1 passing

--- product/nothinbutdotnetstore/infrastructure/containers/basic/DefaultDependencyContainer.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/DefaultDependencyContainer.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class DefaultDependencyContainer : DependencyContainer
     {
+        IDictionary<DependencyType, DependencyFactory> dependency_dictionary;
+
+        public DefaultDependencyContainer(IDictionary<DependencyType, DependencyFactory> dependency_dictionary  )
+        {
+            this.dependency_dictionary = dependency_dictionary;
+        }
+
         public Dependency an<Dependency>()
         {
-            throw new NotImplementedException();
+            foreach(DependencyType dt in dependency_dictionary.Keys)
+            {
+                if (dt.represents(typeof(Dependency)))
+                    return (Dependency)dependency_dictionary[dt].create();
+            }
+          
+            throw new ArgumentException("could not find dependancy");
         }
     }
 }
